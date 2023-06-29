54d1c3269b41cf488840bb0a54c77f4c2d6be484
Ivo1214 <ivo1214@live.com>
2023-06-29 17:44:14 -0300

    Ejercicio resuelto

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -2,3 +2,45 @@
 
 console.log("----------------------------------------");
 console.log("Fetch Poke JSON");
+
+const leerDatosPokemon = async (ruta) => {
+    let infoPokemon = await fetch(ruta);
+    if (!infoPokemon.ok) {
+        // throw parara la ejecucion de la funcion
+        throw new Error (infoPokemon.status);
+    } else {
+        console.log("Datos leidos correctamente.");
+        infoPokemon = await infoPokemon.json();
+        return infoPokemon;
+    }
+};
+
+const procesarDatos = async () => {
+    const listaPokemons = await leerDatosPokemon("./bulbasaur.json");
+    const nombre = listaPokemons.name.toUpperCase();
+    const tipos = listaPokemons.types;
+    const estadisticas = listaPokemons.stats;
+    let acumulador = "";
+
+    // Nombre
+    document.getElementById("pokeName").innerHTML = `<h2>${nombre}</h2>`;
+
+    // Tipos
+    tipos.forEach(element => {
+        acumulador += `<h3>${element.type.name}</h3>`;
+    });
+    document.getElementById("pokeTypes").innerHTML = acumulador;
+
+    // Estadisticas
+    acumulador = "";
+    estadisticas.forEach(element => {
+        acumulador += `<div>
+                        <p>${element.stat.name}</p>
+                        <p>${element.base_stat}</p>
+                    </div>
+                    `;
+    });
+    document.getElementById("pokeStats").innerHTML = acumulador;
+};
+
+procesarDatos ();
